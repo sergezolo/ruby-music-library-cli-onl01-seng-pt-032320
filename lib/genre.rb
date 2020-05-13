9fcee29b6b6177ad60d5e587d5d88cab5df02566
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-05-13 23:11:26 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+class Genre
+
+  attr_accessor :name, :songs
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap do |genre|
+      genre.save
+    end
+  end
+
+end
